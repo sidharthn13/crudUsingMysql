269c14af1c407df1ea06630f6b87e8d0b4da6195
sidharth <sidharth.nair@neoito.com>
2023-11-20 19:13:47 +0530

    read and get features added.

--- middleware.js
+++ middleware.js
@@ -1,44 +1,52 @@
-
-const{body,param,validationResult} = require("express-validator")
-const validateInput  = [
-    param('id').toInt(),
-body().custom((payload,{req})=>{
-    const keys = Object.keys(payload)
-    if (keys.length!=3){throw new Error("Input should have exactly 3 key value pairs");}
-    if (!payload.name) {
+const { body, param, validationResult } = require("express-validator");
+const validateInput = [
+  param("id").toInt(),
+  body()
+    .custom((payload, { req }) => {
+      const keys = Object.keys(payload);
+      if (keys.length != 3) {
+        throw new Error("Input should have exactly 3 key value pairs");
+      }
+      if (!payload.name) {
         throw new Error("There must be a name key");
       }
       if (!payload.age) {
-        throw new Error('There must be an age key');
+        throw new Error("There must be an age key");
       }
       if (!payload.email) {
-        throw new Error('There must be an age key');
+        throw new Error("There must be an age key");
       }
-      return true
-}).bail(),
-body().custom((payload, { req }) => {
-    const nameRegex = /^[a-zA-Z\s]+$/;
-    if (!nameRegex.test(payload.name)) {
-      throw new Error("Invalid name format");
-    }
-    return true;
-  }).bail(),
-body().custom((payload,{req})=>{
-    const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-    if(!gmailRegex.test(payload.email)){
-        throw new Error("Invalid Email format")
-    }
-    return true;
-}).bail(),
-body().custom((payload, { req }) => typeof payload.age === 'number')
-  .withMessage('Age must be strictly a number'),
-  
-(req, res, next) => {
+      return true;
+    })
+    .bail(),
+  body()
+    .custom((payload, { req }) => {
+      const nameRegex = /^[a-zA-Z\s]+$/;
+      if (!nameRegex.test(payload.name)) {
+        throw new Error("Invalid name format");
+      }
+      return true;
+    })
+    .bail(),
+  body()
+    .custom((payload, { req }) => {
+      const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+      if (!gmailRegex.test(payload.email)) {
+        throw new Error("Invalid Email format");
+      }
+      return true;
+    })
+    .bail(),
+  body()
+    .custom((payload, { req }) => typeof payload.age === "number")
+    .withMessage("Age must be strictly a number"),
+
+  (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     next();
   },
-]
-module.exports = {validateInput}
+];
+module.exports = { validateInput };
--- server.js
+++ server.js
@@ -1,64 +1,90 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mysql = require("mysql")
-const {validateInput} = require("./middleware.js")
-
-//middleware to parse JSON payload
-app.use(express.json());
-
+const mysql = require("mysql");
+const { validateInput } = require("./middleware.js");
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'sidharth',
-    password:'sidharth1311',
-    database:'crud'
+  host: "localhost",
+  user: "sidharth",
+  password: "sidharth1311",
+  database: "crud",
 });
-connection.connect((err)=>{if(!err){console.log('connected to DB')}});
 
-app.post("/users",validateInput,(req,res)=>{
-    const name = req.body.name;
-    const age = req.body.age;
-    const email = req.body.email
-    createUser(name,age,email);
-    res.status(200).end('account created');})
+app.use(express.json()); //middleware to parse JSON payload
+connection.connect((err) => {
+  if (!err) {
+    console.log("connected to DB");
+  }
+});
+app.post("/users", validateInput, (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const email = req.body.email;
+  createUser(name, age, email);
+  res.status(200).end("account created");
+});
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const sqlQuery = `SELECT * FROM crud.users WHERE id = ${id}`;
+  connection.query(sqlQuery, (error, results) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    res.status(200).json(results);
+  });
+});
+app.get("/users", (req, res) => {
+  const sqlQuery = `SELECT * FROM crud.users`;
+  connection.query(sqlQuery, (error, results) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    res.status(200).json(results);
+  });
+});
+app.put("/users/:id", validateInput, (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const age = req.body.age;
+  const email = req.body.email;
+  updateUser(id, name, age, email);
+  res.end(`updated ${req.params.id}`);
+});
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  deleteUser(id);
+  res.end(`deleted ${req.params.id}`);
+});
+app.listen(3000, () => {
+  console.log("server instance listening at port 3000");
+});
 
-app.get("/users/:id",(req,res)=>{res.end(`retrieved ${req.params.id}`)})
-
-app.get("/users",(req,res)=>{res.end(`retrieved all data`)})
-
-app.put("/users/:id",validateInput,(req,res)=>{
-    const id = req.params.id;
-    const name = req.body.name;
-    const age = req.body.age;
-    const email = req.body.email;
-    updateUser(id,name,age,email);
-    res.end(`updated ${req.params.id}`);})
-
-app.delete("/users/:id",(req,res)=>{
-    const id = req.params.id;
-    deleteUser(id);
-    res.end(`deleted ${req.params.id}`);})
-
-app.listen(3000,()=>{console.log('server instance listening at port 3000')});
-
-function createUser(name,age,email){
-    const sqlQuery = `INSERT INTO users (name,age,email) VALUES ("${name}",${age},"${email}");`
-    connection.query(sqlQuery,(error,result)=>{
-        if(error){console.log(error);}
-    })
+function createUser(name, age, email) {
+  const sqlQuery = `INSERT INTO users (name,age,email) VALUES ("${name}",${age},"${email}");`;
+  connection.query(sqlQuery, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 }
 
-function deleteUser(id){
-    const sqlQuery = `DELETE FROM users WHERE id = ${id};`
-    connection.query(sqlQuery,(error,result)=>{
-        if(error){console.log(error)}
-    })
+function deleteUser(id) {
+  const sqlQuery = `DELETE FROM users WHERE id = ${id};`;
+  connection.query(sqlQuery, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 }
 
-function updateUser(id,name,age,email){
-    const sqlQuery = `UPDATE users SET name = ?, age = ?, email = ? WHERE
-    id = ? `
-    newData = [name,age,email,id]
-    connection.query(sqlQuery,newData,(error,result)=>{
-        if(error){console.log(error)}
-    })
+function updateUser(id, name, age, email) {
+  const sqlQuery = `UPDATE users SET name = ?, age = ?, email = ? WHERE
+    id = ? `;
+  newData = [name, age, email, id];
+  connection.query(sqlQuery, newData, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 }
