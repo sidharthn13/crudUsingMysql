70cffc9e518a40038c0b953e822e19ba44f55ed6
sidharth <sidharth.nair@neoito.com>
2023-11-20 15:50:28 +0530

    input validation middleware implemented successfully

--- middleware.js
+++ middleware.js
@@ -1,2 +1,33 @@
-const validation  = function(){console.log("hello")}
-module.exports = {validation}
+
+const{body,param,validationResult} = require("express-validator")
+const validateInput  = [
+body().custom((payload,{req})=>{
+    const keys = Object.keys(payload)
+    if (keys.length!=2){throw new Error("Input should have exactly 2 key value pairs");}
+    if (!payload.name) {
+        throw new Error("There must be a name key");
+      }
+      if (!payload.age) {
+        throw new Error('There must be an age key');
+      }
+      return true
+}).bail(),
+body().custom((payload, { req }) => {
+    const nameRegex = /^[a-zA-Z\s]+$/;
+    if (!nameRegex.test(payload.name)) {
+      throw new Error("Invalid name format");
+    }
+    return true;
+  }).bail(),
+body().custom((payload, { req }) => typeof payload.age === 'number')
+  .withMessage('Age must be strictly a number'),
+  
+(req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+]
+module.exports = {validateInput}
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express")
+const app = express();
 const mysql = require("mysql")
+const {validateInput} = require("./middleware.js")
+
+//middleware to parse JSON payload
+app.use(express.json());
 
 const connection = mysql.createConnection({
     host:'localhost',
@@ -7,12 +12,23 @@ const connection = mysql.createConnection({
     password:'sidharth1311',
     database:'crud'
 });
-connection.connect((err)=>{if(!err){console.log('connected')}});
+connection.connect((err)=>{if(!err){console.log('connected to DB')}});
 
 const createQuery = `
 INSERT INTO users (name, age) VALUES ( "devang", 23)`
 
+/*
 connection.query(createQuery,(err,result)=>{
     if(err){console.log(`error:${err}`);}
     else{console.log('query injected into workspace')}
 });
+*/
+
+
+app.post("/users",validateInput,(req,res)=>{res.end('account created')})
+app.get("/users/:id",(req,res)=>{res.end(`retrieved ${req.params.id}`)})
+app.get("/users",(req,res)=>{res.end(`retrieved all data`)})
+app.put("/users/:id",validateInput,(req,res)=>{res.end(`updated ${req.params.id}`)})
+app.delete("/users/:id",(req,res)=>{res.end(`deleted ${req.params.id}`)})
+
+app.listen(3000,()=>{console.log('server instance listening at port 3000')});
