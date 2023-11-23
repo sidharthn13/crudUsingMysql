a878c11cf2d53928eb4fc4b9f981c183832883a9
sidharth <sidharth.nair@neoito.com>
2023-11-23 11:13:02 +0530

    route added for adding data into customer tables in DB

--- server.js
+++ server.js
@@ -11,18 +11,15 @@ const connection = mysql.createConnection({
   database: process.env.DB,
 });
 
-
 //using sequelize
-const { Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
 
-const sequelize = require('./config/database.js');
+const sequelize = require("./config/database.js");
 
 const customers = require("./models/customers.js");
 const orders = require("./models/orders.js");
 const products = require("./models/products.js");
-const orderItems = require("./models/orderItems.js")
-
-
+const orderItems = require("./models/orderItems.js");
 
 app.use(express.json()); //middleware to parse JSON payload
 connection.connect((err) => {
@@ -90,37 +87,65 @@ app.delete("/users/:id", (req, res) => {
   res.end(`deleted ${req.params.id}`);
 });
 
-app.get("/customers/ledger",async(req,res)=>{
+app.get("/customers/ledger", async (req, res) => {
   const joinQuery = `
   
 SELECT CONCAT(c.firstName, ' ',c.lastName) AS FullName ,o.OrderDate AS dateOfOrder ,p.productName, p.price AS pricePerUnit,oi.quantity, oi.quantity * p.price AS totalAmount   
 FROM customers c INNER JOIN orders o ON c.customerID = o.customerID
 INNER JOIN orderItems oi ON o.orderID = oi.orderID INNER JOIN products p ON oi.productID = p.productID;
-`
-try{
-  await sequelize.query(`USE learningSequelize;`)
-const result = await sequelize.query(joinQuery,{ type: Sequelize.QueryTypes.SELECT });
-res.status(200).json(result)
-}
-catch(error){
-  console.log(`Could not perform join operation:${error}`)
-  res.status(500).json({'error':error})
-}
+`;
+  try {
+    await sequelize.query(`USE learningSequelize;`);
+    const result = await sequelize.query(joinQuery, {
+      type: Sequelize.QueryTypes.SELECT,
+    });
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(`Could not perform join operation:${error}`);
+    res.status(500).json({ error: error });
+  }
+});
 
+app.post("/customers", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  try {
+    const [result, accountCreated] = await customers.findOrCreate({
+      where: { email: data.email },
+      defaults: {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        country: data.country,
+      },
+    });
+    if (!accountCreated) {
+      throw Error("customer is already registered in database");
+    }
+    res.status(200).end("Customer details added");
+  } catch (error) {
+    res.status(400).end(error.message);
+  }
 });
 
 //sequelize ORM instance used inside call back function of server.listen
 
-  app.listen(3000, () => {
-    console.log("server instance listening at port 3000");
-    
-      customers.sync().then((res)=>{console.log("customer table created")});
-      orders.sync().then((res)=>{console.log("orders table created")});
-      products.sync().then((res)=>{console.log("products table created")});
-      orderItems.sync().then((res)=>{console.log("orderItems table created")});
-      //generate_rows()
-  });
+app.listen(3000, () => {
+  console.log("server instance listening at port 3000");
 
+  customers.sync().then((res) => {
+    console.log("customer table created");
+  });
+  orders.sync().then((res) => {
+    console.log("orders table created");
+  });
+  products.sync().then((res) => {
+    console.log("products table created");
+  });
+  orderItems.sync().then((res) => {
+    console.log("orderItems table created");
+  });
+  //generate_rows()
+});
 
 function deleteUser(id) {
   const sqlQuery = `DELETE FROM users WHERE id = ${id};`;
@@ -148,35 +173,48 @@ function updateUser(userDetails) {
 }
 
 //using bulk create to insert data
-async function generate_rows(){
+async function generate_rows() {
   const customersData = [
-    { firstName: 'John', lastName: 'Doe', email: 'john.doe@email.com', country: 'USA' },
-    { firstName: 'Jane', lastName: 'Smith', email: 'jane.smith@email.com', country: 'Canada' },
-    { firstName: 'andy', lastName: 'gold', email: 'andy.gold@email.com', country: 'UK' },
+    {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@email.com",
+      country: "USA",
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      email: "jane.smith@email.com",
+      country: "Canada",
+    },
+    {
+      firstName: "andy",
+      lastName: "gold",
+      email: "andy.gold@email.com",
+      country: "UK",
+    },
   ];
   const ordersData = [
-    {customerID:1, OrderDate:'2023-01-15',TotalAmount: 150.00},
-    {customerID:2, OrderDate:'2023-02-20',TotalAmount: 200.50},
-    {customerID:3, OrderDate:'2023-03-10',TotalAmount: 75.25},
+    { customerID: 1, OrderDate: "2023-01-15", TotalAmount: 150.0 },
+    { customerID: 2, OrderDate: "2023-02-20", TotalAmount: 200.5 },
+    { customerID: 3, OrderDate: "2023-03-10", TotalAmount: 75.25 },
   ];
   const productsData = [
-    {productName:'laptop',price:800.00},
-    {productName:'smartphone',price:400.00},
-    {productName:'headphones',price:50.00},
+    { productName: "laptop", price: 800.0 },
+    { productName: "smartphone", price: 400.0 },
+    { productName: "headphones", price: 50.0 },
   ];
   const orderItemsData = [
-    {orderID:1,productID:1,quantity:2,pricePerUnit:800.00},
-    {orderID:2,productID:2,quantity:1,pricePerUnit:400.00},
-    {orderID:3,productID:3,quantity:3,pricePerUnit:50.00},
-  ]
-  try{
-  const customerTableResult = await customers.bulkCreate(customersData);
-  const ordersTableResult = await orders.bulkCreate(ordersData);
-  const productsTableResult = await products.bulkCreate(productsData);
-  const orderItemsTableResult = await orderItems.bulkCreate(orderItemsData);
-  }
-  catch(error){
-    console.log(`warning, error: ${error}`)
+    { orderID: 1, productID: 1, quantity: 2, pricePerUnit: 800.0 },
+    { orderID: 2, productID: 2, quantity: 1, pricePerUnit: 400.0 },
+    { orderID: 3, productID: 3, quantity: 3, pricePerUnit: 50.0 },
+  ];
+  try {
+    const customerTableResult = await customers.bulkCreate(customersData);
+    const ordersTableResult = await orders.bulkCreate(ordersData);
+    const productsTableResult = await products.bulkCreate(productsData);
+    const orderItemsTableResult = await orderItems.bulkCreate(orderItemsData);
+  } catch (error) {
+    console.log(`warning, error: ${error}`);
   }
 }
-
